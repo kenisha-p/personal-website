06a1ed843a5979ab3c1efddb1ec7ed08575c666b
Kenisha <k4perera@uwaterloo.ca>
2025-01-11 18:52:30 -0500

    add changes

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import {
-  Col,
-} from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 const ExperienceCard = ({ data }) => {
   return (
     <Col lg="6">
       <div className="pb-5 text-center">
-        <img className=" bg-white mb-3" src={data.companylogo} alt="" />
+        <div className="d-flex justify-content-center align-items-center">
+          <img
+            className="bg-white mb-3"
+            src={data.companylogo}
+            alt=""
+            style={{ width: "50px", height: "50px" }}
+          />
+          <p className="lead mb-0 ms-3">{data.companyname}</p>
+        </div>
         <p className="lead">
           {data.role}
           <br />
           {data.date}
         </p>
-
       </div>
     </Col>
   );
-}
+};
 
 export default ExperienceCard;
